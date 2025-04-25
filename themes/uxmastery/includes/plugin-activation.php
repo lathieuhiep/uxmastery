b5469e999c7b2332c4843e411fb3d6fe33dfb566
lathieuhiep <khacdiepkma90@gmail.com>
2025-04-25 23:29:46 +0700

    update code

--- themes/uxmastery/includes/plugin-activation.php
+++ themes/uxmastery/includes/plugin-activation.php
@@ -13,6 +13,14 @@ function uxmastery_register_required_plugins(): void {
 	$uxmastery_plugins = array(
 		// This is an example of how to include a plugin from the WordPress Plugin Repository
 		array(
+			'name'      =>  'Breadcrumb Navxt',
+			'slug'      =>  'breadcrumb-navxt',
+			'required'  =>  true,
+			'source' => 'https://github.com/Codestar/codestar-framework/archive/refs/heads/master.zip'
+		),
+
+		// This is an example of how to include a plugin from the WordPress Plugin Repository
+		array(
 			'name'      =>  'Codestar Framework',
 			'slug'      =>  'codestar-framework',
 			'required'  =>  true,
