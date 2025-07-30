4bcf374176ad832e2fad7ed984da9f28a3e93a57
lathieuhiep <khacdiepkma90@gmail.com>
2025-07-30 22:11:26 +0700

    change fonts

--- themes/uxmastery/header.php
+++ themes/uxmastery/header.php
@@ -5,6 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="<?php bloginfo('description'); ?>" />
+    <!-- reconnect and preload for Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" onload="this.onload=null;this.rel='stylesheet'">
+    <noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"></noscript>
+
     <?php wp_head(); ?>
 </head>
 <body <?php body_class('body-scroll'); ?>>
